10a5ef7a782102a56866f64751753938d9e72e74
Matthew Nelson <developer@matthewnelson.io>
2020-08-04 18:22:39 -0400

    implement TorServiceController.Builder and startTor api call

--- android/app/src/main/java/com/zeus/MainApplication.java
+++ android/app/src/main/java/com/zeus/MainApplication.java
@@ -16,13 +16,21 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import android.content.Context;
 
+import androidx.core.app.NotificationCompat;
+
 import com.reactlibrary.securekeystore.RNSecureKeyStorePackage;
 
 import com.facebook.soloader.SoLoader;
 import com.microsoft.ActivityResultPackage;
+import com.zeus.topl_android.ZeusTorEventBroadcaster;
+import com.zeus.topl_android.ZeusTorSettings;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+
+import io.matthewnelson.topl_service.TorServiceController;
+import io.matthewnelson.topl_service.lifecycle.BackgroundManager;
+import io.matthewnelson.topl_service.notification.ServiceNotification;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -54,5 +62,66 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+
+    // setup Tor first before calling any APIs (otherwise RunTimeException is thrown)
+    setupTor(this);
+
+    // put this anywhere (maybe after successful login)
+    TorServiceController.Companion.startTor();
+
+    // other APIs (for now ;-D)
+//    TorServiceController.Companion.stopTor();
+//    TorServiceController.Companion.restartTor();
+//    TorServiceController.Companion.newIdentity();
+  }
+
+  private ServiceNotification.Builder generateServiceNotificationBuilder() {
+      return new ServiceNotification.Builder(
+              "ZeusLN Tor",
+              "TorService",
+              "TorOnionProxyLibrary-Android",
+              615
+      )
+              // customize the images later with your own images
+              .setImageTorNetworkingEnabled(R.drawable.tor_stat_network_enabled)
+              .setImageTorNetworkingDisabled(R.drawable.tor_stat_network_disabled)
+              .setImageTorDataTransfer(R.drawable.tor_stat_network_dataxfer)
+              .setImageTorErrors(R.drawable.tor_stat_notifyerr)
+
+              // choose a color that you like more
+              .setCustomColor(R.color.purple)
+
+              // lock screen visibility
+              .setVisibility(NotificationCompat.VISIBILITY_SECRET)
+
+              // notification buttons (New ID is always present)
+              .enableTorRestartButton(true)
+              .enableTorStopButton(true)
+
+              // enable/disable showing of notification
+              .showNotification(true);
+  }
+
+  private BackgroundManager.Builder.Policy generateBackgroundManagerPolicy() {
+      return new BackgroundManager.Builder()
+              .respectResourcesWhileInBackground(20);
+  }
+
+  private void setupTor(Application application) {
+      new TorServiceController.Builder(
+              application,
+              generateServiceNotificationBuilder(),
+              generateBackgroundManagerPolicy(),
+              BuildConfig.VERSION_CODE,
+              new ZeusTorSettings(),
+              "common/geoip",
+              "common/geoip6"
+      )
+              .setBuildConfigDebug(BuildConfig.DEBUG)
+
+              // is available from TorServiceController.Companion.appEventBroadcaster
+              // just cast it as ZeusTorEventBroadcaster
+              .setEventBroadcaster(new ZeusTorEventBroadcaster())
+              .build();
   }
 }
